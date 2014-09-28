1bfa18498c1c5543217415c1843a98fbad63838e
Chris Stier <john.c.stier@gmail.com>
2014-09-27 21:38:38 -0600

    Small changed to usage check.

--- jaws-central/src/main/java/co/riverrunners/jaws/App.java
+++ jaws-central/src/main/java/co/riverrunners/jaws/App.java
@@ -17,7 +17,7 @@ public class App {
 
 
     public static void main(String[] args) {
-        if(args.length>0 && !args[0].isEmpty()){
+        if(args.length == 1 && !args[0].isEmpty()){
             if(args[0].equals("--help")){
                 printUsage();
                 return;
